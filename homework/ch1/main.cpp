559c128af0df40d9147365f881e1b48a339741ef
jrchen <jrchen@nreal.ai>
2023-03-13 17:46:18 +0800

    eigen pangolin exp

---
+++ homework/ch1/main.cpp
@@ -0,0 +1,8 @@
+#include "hello.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    SayHello();
+    return 0;
+}
---
+++ homework/ch1/src/hello.cpp
@@ -0,0 +1,8 @@
+#include "hello.h"
+#include <iostream>
+
+using namespace std;
+
+void SayHello() {
+    cout << "Hello SLAM!" << endl;
+}
